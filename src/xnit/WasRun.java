43836d1c4733f92b0a6fb8d13f7922509b176797
dhdydtn2019 <dhdydtn2019@gmail.com>
2020-05-16 23:49:44 +0900

    WasRun 메서드 구현하여 테스팅 기초 만들기

---
+++ src/xnit/WasRun.java
@@ -0,0 +1,26 @@
+package xnit;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class WasRun {
+    private final String name;
+    public boolean wasRun;
+
+    public WasRun(String name) {
+        this.name = name;
+    }
+
+    public void testMethod() {
+        wasRun = true;
+    }
+
+    public void run() {
+        try {
+            Method method = getClass().getMethod(name); // Method 정보 불러오기 (name과 같은 이름을 가진 메소드를 클래스 내에서 가져온다.)
+            method.invoke(this); // 가져온 메서드를 불러온다.
+        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
