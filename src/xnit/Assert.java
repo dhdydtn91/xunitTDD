b05a8552979a712512224111097f32525a4c4f77
dhdydtn2019 <dhdydtn2019@gmail.com>
2020-05-17 00:04:29 +0900

    테스트 메서드 실행

---
+++ src/xnit/Assert.java
@@ -0,0 +1,8 @@
+package xnit;
+
+public class Assert {
+    public static void assertEquals(Object expected, Object actual) {
+        if (!expected.equals(actual))
+            throw new AssertionError("expected < " + expected + "> but was <" + actual + ">");
+    }
+}
